9b348e31cebf82171be235b4264448892880ab2f
Tejas Birla <tejasbirla3@gmail.com>
2026-09-16 13:01:32 +0530

    db connection string added.

--- server/db.py
+++ server/db.py
@@ -1,12 +1,10 @@
+import os
 import psycopg
+from dotenv import load_dotenv
 from psycopg.rows import dict_row
 
-conn = psycopg.connect(
-    dbname="task_manager",
-    user="tejasbirla3gmail.com",
-    host="/tmp",
-    port=5432,
-    row_factory=dict_row,
-)
+load_dotenv()
+
+conn = psycopg.connect(os.getenv("DATABASE_URL"), row_factory=dict_row)
 
 print("Database connected successfully.")
